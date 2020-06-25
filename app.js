925165067cee30b8e1f1bd86ba47798c162866d9
alynch-code <athinelynch@icloud.com>
2020-06-25 14:56:14 -0500

    First commit of a To Do List App.

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const app = express();
+const items = ["Code","Workout","Eat Food"];
+const workItems = [];
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  let day = date.getDate();
+  res.render("lists", {
+    listTitle: day,
+    newListItems: items,
+
+  })
+});
+
+app.get("/work", function(req, res) {
+  res.render("lists", {
+    listTitle: "Work List",
+    newListItems: workItems,
+  })
+});
+
+app.post("/",function(req,res){
+  const item = req.body.newListItem
+  if(req.body.list === "Work List"){
+    workItems.push(item)
+    res.redirect("/work")
+  }else{
+    items.push(item)
+    res.redirect("/")
+  }
+});
+
+app.post("/work", function(req,res){
+  const item =req.body.newListItem
+  workItems.push(item)
+  res.redirect("/work")
+});
+
+app.listen(8080, function() {
+  console.log("Listening...")
+});
